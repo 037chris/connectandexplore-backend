50e39317dcf6af896752c5161eb8c7f549857829
s88268 <s88268@bht-berlin.de>
2023-11-11 00:03:03 +0100

    userModel test

--- Backend/tests/model/UserModel.test.ts
+++ Backend/tests/model/UserModel.test.ts
@@ -1,14 +1,7 @@
 import { clearDatabase, closeDatabase, connect } from "../../database/db";
 import { IAddress, IUser, User } from "../../src/model/UserModel";
 
-
-describe("userModel test", () => {
-    beforeAll(async () => await connect());
-    afterEach(async () => await clearDatabase());
-    afterAll(async () => await closeDatabase());
-
-    test("create User", async () => {
-        const a:IAddress = {
+const a:IAddress = {
             street: "Street",
             houseNumber: "1",
             postalCode: "12345",
@@ -25,11 +18,48 @@ describe("userModel test", () => {
             isAdministrator: true,
             address: a,
             birthDate: new Date(),
-            gender: "male"
+            gender: "male",
+            isActive:true,
+            profilePicture:"picture1",
+            socialMediaUrls:{
+                facebook:"facebook",
+                instagram:"instagram"
+            }
         }
+
+describe("userModel test", () => {
+    beforeAll(async () => await connect());
+    afterEach(async () => await clearDatabase());
+    afterAll(async () => await closeDatabase());
+
+    test("create User", async () => {
         const user = await User.create(u);
+        expect(user.id).toBeDefined();
         expect(user.name.first).toBe(u.name.first);
         expect(user.name.last).toBe(u.name.last);
-        
-    })
+        expect(user.email).toBe(u.email);
+        expect(user.password).not.toBe(u.password);
+        expect(user.password).toBeDefined();
+        expect(await user.isCorrectPassword("123")).toBeTruthy();
+        expect(user.address).toMatchObject(a);
+        expect(user.birthDate).toBe(u.birthDate);
+        expect(user.gender).toBe(u.gender);
+        expect(user.isActive).toBeTruthy();
+        expect(user.profilePicture).toBe(u.profilePicture);
+        expect(user.socialMediaUrls).toMatchObject(u.socialMediaUrls);
+    });
+
+    test("updated user password middleware", async ()=>{
+        const user = await User.create(u);
+        user.password = "456";
+        user.email = "John@some-host.de";
+        await user.save();
+        const res = await User.findById(user.id);
+        expect(res.password).not.toBe("456");
+        expect(res.password).not.toBe("123");
+        expect(res.password).toBeDefined();
+        expect(await res.isCorrectPassword("456")).toBeTruthy();
+        expect(res.email).toBe("John@some-host.de");
+        expect(await User.findOne({email:"John@doe.com"})).toBeNull();
+    });
 })
