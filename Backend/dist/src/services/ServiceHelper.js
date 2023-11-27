70af140e25184b78977597ed1d92e01b580c07ac
s88268 <s88268@bht-berlin.de>
2023-11-27 19:03:19 +0100

    ups

--- Backend/dist/src/services/ServiceHelper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateToString = void 0;
-function dateToString(date) {
-    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-}
-exports.dateToString = dateToString;
-//# sourceMappingURL=ServiceHelper.js.map
